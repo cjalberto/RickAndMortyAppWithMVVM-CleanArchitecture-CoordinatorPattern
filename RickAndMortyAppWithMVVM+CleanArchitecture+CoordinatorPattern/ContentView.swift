c192ea540927c87f384a57ca4564541f8266794a
Carlos Jaramillo <cjalberto1503@gmail.com>
2024-01-06 14:31:40 -0500

    starting project

---
+++ RickAndMortyAppWithMVVM+CleanArchitecture+CoordinatorPattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyAppWithMVVM+CleanArchitecture+CoordinatorPattern
+//
+//  Created by Carlos Jaramillo on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyAppWithMVVM+CleanArchitecture+CoordinatorPattern/RickAndMortyAppWithMVVM_CleanArchitecture_CoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppWithMVVM_CleanArchitecture_CoordinatorPatternApp.swift
+//  RickAndMortyAppWithMVVM+CleanArchitecture+CoordinatorPattern
+//
+//  Created by Carlos Jaramillo on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppWithMVVM_CleanArchitecture_CoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
